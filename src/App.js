d9845da44972e88d5f3cd263431ddea10a31e475
Ajay Shirke <ajay.shirke@fugetroncorp.com>
2022-06-22 22:57:55 +0530

    all changes

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <p>Here We Go Git Modification And Staging Tutorial</p>
+       <p>Git Logs: To Track All Commits</p>
+       <p>take it as new changes</p>
+       <p>Git Status: To  Areas</p>
+       New Sudden Modification
+      </header>
+    </div>
+  );
+}
+
+export default App;
